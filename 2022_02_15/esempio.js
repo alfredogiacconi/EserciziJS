262d4241d54b96a70ddbc7b4d1c038e899fee4ab
alfredogiacconi <alfredo.giacconi@gmail.com>
2022-02-15 17:01:17 +0100

    function createTable

--- 2022_02_15/esempio.js
+++ 2022_02_15/esempio.js
@@ -1,5 +1,35 @@
 // Commento su riga
 
-function ciao() {
-    alert('Ciao!')
-};
+/**
+ * document.getElementById
+ * document.getElementsByTagName
+ * document.createElement
+ * 
+ * ___.parentNode
+ * ___.childNodes
+ * ___.lastChild
+ * 
+ * ___.nextSibling
+ * ___.previousSibling
+ * 
+ * ___.getAttribute
+ * ___.setAttribute("..." , "...")
+ * 
+ * ___.appendChild
+ */
+
+ function ciao() {
+    alert('Ciao!');
+}
+
+function createTable() {
+    body = document.body;
+    let table = document.createElement("table"); // tabella creata e allocata in memoria
+    let tbody = document.createElement("tbody");
+    let trow = document.createElement("trow");
+    body.appendChild(table);
+    table.appendChild(tbody);
+    tbody.appendChild(trow);
+
+    document.getElementsByTagName("body")[0].appendChild(table); // [0] -> prendi il primo elemento dell indice col il tag body
+}
